3fe59cac846d5b5da5b9a0e49871ed4fb28feb03
c-mac88 <christianmcfarland@gmail.com>
2016-08-17 08:32:21 -0700

    initial commit

---
+++ TicTacToeWithAI.py
@@ -0,0 +1,14 @@
+import sys
+if sys.version_info[0] == 2:
+    input = raw_input # python 2 compatibility
+print('   |   |   ')
+print('   |   |   ')
+print('   |   |   ')
+print('---+---+---')
+print('   |   |   ')
+print('   |   |   ')
+print('   |   |   ')
+print('---+---+---')
+print('   |   |   ')
+print('   |   |   ')
+print('   |   |   \n')
